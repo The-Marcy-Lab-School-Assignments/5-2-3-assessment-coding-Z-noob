5f839d05e819762ad77ae2b459b2df6e6b0d4b13
Z-noob <Zhenniwu5@gmail.com>
2024-07-18 11:21:25 -0400

    2-factory part

--- src/2-factory-function.js
+++ src/2-factory-function.js
@@ -1,6 +1,22 @@
-const bankAccountFactory = () => {
-
+const bankAccountFactory = (balance) => {
+    return {
+      checkBalance() {
+        if(!balance) {return 0};
+        return parseInt(balance);
+      },
+      add(toAdd) {
+        console.log(`$${toAdd} added.`);
+        return balance += toAdd;
+      },
+      subtract(toSubtract) {
+        console.log(`$${toSubtract} subtracted.`);
+        return balance -= toSubtract;
+      }
+    }
 };
+// console.info(typeof(bankAccountFactory()))
+// console.info(Object.keys(bankAccountFactory()))
+
 
 module.exports = {
   bankAccountFactory,
--- src/playground.js
+++ src/playground.js
@@ -1,8 +1,24 @@
-const ToDoItem = require('./ToDoItem');
-const ToDoList = require('./ToDoList');
-
+// const ToDoItem = require('./ToDoItem');
+// const ToDoList = require('./ToDoList');
+// const {
+//   bankAccountFactory,
+//   Phone,
+//   AppleIPhone,
+// } = require('./from-scratch');
+const ToDoItem = require('./0-has-many-belongs-to');
+const ToDoList = require('./0-has-many-belongs-to');
 const {
   bankAccountFactory,
+} = require('./2-factory-function');
+const {
   Phone,
-  AppleIPhone,
-} = require('./from-scratch');
+  AppleIPhone
+} = require('./1-inheritance-polymorphism')
+
+const myAccount = bankAccountFactory(1000)
+// console.log(Object.keys(bankAccountFactory()))
+console.log(myAccount.checkBalance())
+myAccount.add(200);
+console.log(myAccount.checkBalance())
+myAccount.add(800);
+console.log(myAccount.checkBalance())
